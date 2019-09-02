83d438523cb28b28ad43bc5fb2585f7057d23dfc
Sergei Pintus <sspintus@biosoft.ru>
2019-09-02 18:44:13 +0700

    rna-seq-pipeline

---
+++ rna-seq-pipeline.sh
@@ -0,0 +1,36 @@
+trim() {
+  echo "Trimming..."
+}
+
+align() {
+  echo "Aligning..."
+}
+
+count(){
+  echo "Counting..."
+}
+
+deg(){
+  echo "Differential Expression..."
+}
+
+go(){
+  echo "GO Enrichment..."
+}
+
+tf(){
+  echo "TF Enrichment..."
+}
+
+path(){
+  echo "Pathway Enrichment..."
+}
+
+trim
+align
+count
+deg
+go
+tf
+path
+
