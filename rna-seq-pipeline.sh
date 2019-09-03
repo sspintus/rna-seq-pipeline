663c81806dbbcb3c47399a110363606d1dd70410
Sergei Pintus <sspintus@biosoft.ru>
2019-09-03 12:24:28 +0700

    tools & qc steps

--- rna-seq-pipeline.sh
+++ rna-seq-pipeline.sh
@@ -1,8 +1,24 @@
-trim() {
-  echo "Trimming..."
+fastq_dir=$1
+tools_dir=$2
+fastqc=$tools_dir'/FastQC/fastqc'
+trimmomatic='java - jar '$tools_dir'/Trimmomatic-0.36/trimmomatic-0.36.jar'
+hisat=$tools_dir'/hisat2-2.1.0/hisat2-align'
+htseq=$tools_dir'/HTSeq/htseq-count'
+edger='Rscript '$tools_dir'/edgeR/edgeR.R'
+
+qc_raw(){
+  echo "QC on raw data..."
 }
 
-align() {
+trim_clip(){
+  echo "Trimming & clipping..."
+}
+
+qc_trim(){
+  echo "QC on trimmed & clpped data"
+}
+
+align(){
   echo "Aligning..."
 }
 
@@ -26,7 +42,9 @@ path(){
   echo "Pathway Enrichment..."
 }
 
-trim
+qc_raw
+trim_clip
+qc_trim
 align
 count
 deg
